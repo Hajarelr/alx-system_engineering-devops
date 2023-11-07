6ae72e01c13232f58f2f72926d47f4fd440e42bc
Hajarelr <hajar.elrh@gmail.com>
2023-11-07 11:50:18 +0000

    Hajar

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -39,7 +39,7 @@ def count_words(subreddit, word_list, instances={}, after="", count=0):
         title = n.get("data").get("title").lower().split()
         for word in word_list:
             if word.lower() in title:
-                times = len([m for m in title if m == word.lower()])
+                times = len([t for t in title if t == word.lower()])
                 if instances.get(word) is None:
                     instances[word] = times
                 else:
